f979194d60dc8d0d80cb1393b5dd629c530f2425
crystalforceix <crystalforceix@gmail.com>
2025-11-22 20:36:15 +0700

    idk

--- temp-code-c/temp.c
+++ temp-code-c/temp.c
@@ -1,18 +1,26 @@
+/* Import libs */
 #include "stdio.h"
 #include "math.h"
 #include "../lib/conio/conio.h"
 #include "../lib/conio/conio.c"
 
+/* Enum */
+enum Weather {Sunny, Rain, Darkness, Lightness, Cold};
+
+/* Main func */
 int main() {
-  enum {T0, T1, T2};
-  enum day {cn, thu2, thu3, thu4, thu5, thu6, thu7} n1;
-  enum day n2;
-  int i, j = 2000, k = T2;
-  clrscr();
-  i = thu7;
-  n1 = -1000;
-  n2 = j;
-  printf("\n n1 = %d  n2 = %d i = %d", n1, n2, i);
-  printf("\n k = %d T1 = %d", k, T1);
-  getch();
-}
+  printf("Hello world");
+};
+
+/* Some stupid func;v */
+int Weather_check() {
+  enum Weather today;
+  today = Rain;
+  if (today == Sunny) {
+    printf("Today's sunny!");
+    }
+  else {
+    printf("Idk xD");
+  };
+  return 0;
+};
